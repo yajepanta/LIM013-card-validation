5aebdc13e050070f665dcd94647ce1f48ea7fd50
YajePanta <yajepanta@gmail.com>
2020-07-16 11:19:36 -0500

    esqueleto en HTML de la 1ra pantalla, agregue boton validar con EventListener en index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+botonValidar.addEventListener("click", funcionValidar);
+
+function funcionValidar() {
+  alert("Es válido");
+}
