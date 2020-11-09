e0fe97cc96a8c6c4e2a845857cbe9d71b0f719f8
YajePanta <yajepanta@gmail.com>
2020-11-08 22:09:30 -0500

    background style

--- src/index.js
+++ src/index.js
@@ -60,6 +60,7 @@ function siguientePagina (a) {
     botonValidar.addEventListener("click", funcionValidar);
 
     let resultados = document.getElementById("resultados");
+    
     var numeroTarjeta = document.getElementById("numeroTarjeta");
 
     let botonBorrar = document.getElementById("botonBorrar");
@@ -126,3 +127,23 @@ function funcionValidar(e) {
 
  }
 }
+
+{/* <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
+
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div> */}
+  
+
+
+
+
+$(document).ready(function(){
+  $('#exampleModal').modal('show');
+}); 
+
+
